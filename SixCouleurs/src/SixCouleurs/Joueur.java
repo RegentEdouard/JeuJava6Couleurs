c3a1d8dc92d1786bfd3fa9841f77f822e77660ee
RegentEdouard <edouard.regent@gmail.com>
2016-03-21 22:40:28 +0100

    Joueur+Plateau
    Développement de la classe Joueur et création de joueur dans la création
    de plateau.

--- SixCouleurs/src/SixCouleurs/Joueur.java
+++ SixCouleurs/src/SixCouleurs/Joueur.java
@@ -2,6 +2,8 @@ package SixCouleurs;
 
 public class Joueur {
 	public boolean ia = false;
+	public char couleur;
+	public boolean[][] territoire;
 
 	public boolean isIa() {
 		return ia;
@@ -9,6 +11,22 @@ public class Joueur {
 
 	public void setIa(boolean ia) {
 		this.ia = ia;
+	}
+
+	public char getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(char couleur) {
+		this.couleur = couleur;
+	}
+
+	public boolean[][] getTerritoire() {
+		return territoire;
+	}
+
+	public void setTerritoire(boolean[][] territoire) {
+		this.territoire = territoire;
 	}
 	
 }
--- SixCouleurs/src/SixCouleurs/Plateau.java
+++ SixCouleurs/src/SixCouleurs/Plateau.java
@@ -1,25 +1,37 @@
 package SixCouleurs;
 
 public class Plateau {
-	public String[][] plateau;
+	public char[][] plateau;
 
-	public String[][] getPlateau() {
+	public char[][] getPlateau() {
 		return plateau;
 	}
 
-	public void setPlateau(String[][] plateau) {
+	public void setPlateau(char[][] plateau) {
 		this.plateau = plateau;
 	}
 	
 	public Plateau(int ligne, int colonne) {
-		String[][] plateau = new String[ligne][colonne];
-		String[] couleur = {"r","o","j","v","b","i"};
-		for (int i=0; i<plateau.length; i++){
-			for(int j=0; j<plateau[0].length; j++){
+		char[][] plateau = new char[ligne][colonne];
+		char[] couleur = {'r','o','j','v','b','i'};
+		for (int i=0; i<ligne; i++){
+			for(int j=0; j<colonne; j++){
 				plateau[i][j] = couleur[alea(0,5)];
 			}
 		}
+		
+		//pour deux joueurs
+		plateau[0][0] = (char)((int)plateau[0][0]-32);	//passage de la case en haut à gauche en majuscule (joueur1)
+		plateau[ligne-1][colonne-1] = (char)((int)plateau[ligne-1][colonne-1]-32);	////passage de la case en bas à droite en majuscule (joueur2)
 		this.plateau = plateau;
+		
+		//création des joueurs
+		Joueur joueur1 = new Joueur();
+		Joueur joueur2 = new Joueur();
+		joueur1.setTerritoire(tabDebut(0, 0, new boolean[ligne][colonne]));
+		joueur1.setCouleur(plateau[0][0]);
+		joueur2.setTerritoire(tabDebut(ligne-1, colonne-1, new boolean[ligne][colonne]));
+		joueur1.setCouleur(plateau[ligne-1][colonne-1]);
 	}
 	
 	public void afficher(){
@@ -35,5 +47,15 @@ public class Plateau {
 	public static int alea(int min, int max){
 		int Nb =(int)( min + (Math.random() * (max - min)));
 		return Nb;
+	}
+	
+	public static boolean[][] tabDebut(int x, int y, boolean[][] tab){
+		for (int i=0; i<tab.length; i++){
+			for (int j=0; j<tab[0].length; j++){
+				tab[i][j] = false;
+			}
+		}
+		tab[y][x] = true;
+		return tab;
 	}
 }
